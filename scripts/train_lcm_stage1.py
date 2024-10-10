c36844b32e97863a52c162862f3ff18e110aaf72
leeway.zlw <leeway.zlw@outlook.com>
2024-10-10 17:23:57 +0800

    update

--- scripts/train_lcm_stage1.py
+++ scripts/train_lcm_stage1.py
@@ -744,7 +744,8 @@ def train_stage1_process(cfg: argparse.Namespace) -> None:
                 # cond_sigmas[:] = 0
                 noise_aug_strength = cond_sigmas[0] # TODO: support batch > 1
                 cond_sigmas = cond_sigmas[:, None, None, None, None]
-                conditional_pixel_values = ref_img
+                # conditional_pixel_values = ref_img
+                conditional_pixel_values = ref_img[:, 0:1, :, :, :]
                 conditional_pixel_values = \
                     torch.randn_like(conditional_pixel_values) * cond_sigmas + conditional_pixel_values
                 conditional_latents = tensor_to_vae_latent(conditional_pixel_values, vae)[:, 0, :, :, :]
@@ -762,13 +763,10 @@ def train_stage1_process(cfg: argparse.Namespace) -> None:
                 # Concatenate the `conditional_latents` with the `noisy_latents`.
                 conditional_latents = conditional_latents.unsqueeze(
                     1).repeat(1, noisy_latents.shape[1], 1, 1, 1)
-                print_tensor_device("conditional_latents", conditional_latents)
                 inp_noisy_latents = torch.cat(
                     [inp_noisy_latents_scale, conditional_latents], dim=2)
-                print_tensor_device("inp_noisy_latents", inp_noisy_latents)
                 inp_noisy_latents_uncond = torch.cat(
                     [inp_noisy_latents_scale, torch.zeros_like(conditional_latents)], dim=2)
-                print_tensor_device("inp_noisy_latents_uncond", inp_noisy_latents_uncond)
                 # encoder_hidden_states_uncond = torch.zeros_like(encoder_hidden_states)
 
                 with torch.no_grad():
